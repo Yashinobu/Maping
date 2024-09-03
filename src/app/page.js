0b0a36d08945d9c49c452e4d98723b92157c7ade
Ellio <sereneheart888@gmail.com>
2024-09-03 12:08:14 +0300

    [U] login

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,15 @@
 'use client'
-import { useWindowHeight } from "@react-hook/window-size";
 import Head from "next/head";
-import Image from "next/image";
-import { useEffect } from "react";
-
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 
-  const height = useWindowHeight();
-  useEffect(() => {
-    const vh = height * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    console.log(`height: ${height}`)
-  }, [height]);
+  const router = useRouter();
+
+  const handleClick = (e) => {
+    router.push('./Login');
+  }
+
   return (
     <>
       <Head>
@@ -26,7 +23,7 @@ export default function Home() {
         <label className="text-[14px] text-[#A5A5A5] mt-[100px]">利用規約</label>
         <div className="flex flex-col w-[80%] mt-[20px] gap-4">
           <button className="bg-[#FAFB64] text-[#707070] text-[18px] px-[32px] font-bold py-[10px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-full">アカウントを作成する</button>
-          <button className="bg-[#A5A5A5] text-[#FFFFFF] text-[18px] px-[32px] font-bold py-[10px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-full">ログイン</button>
+          <button className="bg-[#A5A5A5] text-[#FFFFFF] text-[18px] px-[32px] font-bold py-[10px] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-full" onClick={handleClick}>ログイン</button>
         </div>
         <label className="text-[#A5A5A5] mt-[10px] text-[14px]">ログインできない場合はこちら-</label>
       </main>
